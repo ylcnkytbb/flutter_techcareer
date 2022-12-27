b6dcd2322673ab36c3e6885390b1a047855f3bc1
Tuğba Yalçınkaya <tugbayalcinkaya3437@gmail.com>
2022-12-28 00:01:21 +0300

    odev5_calculator_app

---
+++ odev5_calculator/lib/calculator.dart
@@ -0,0 +1,131 @@
+
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  Widget numButton(String btnText, Color btnColor, Color txtColor) {
+    return ElevatedButton(
+      onPressed: () {
+        calculate(btnText);
+      },
+      child: Text(
+        btnText,
+        style: TextStyle(
+          fontSize: 27,
+          color: txtColor,
+        ),
+      ),
+      style: ElevatedButton.styleFrom(
+        fixedSize: Size(70, 70),
+        shape: CircleBorder(),
+        backgroundColor: btnColor, //bu ayrıntıya dikkat et
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        title: Text('Hesap Makinesi'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    text,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(color: Colors.white, fontSize: 80),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                numButton("7", (Colors.white24)!, Colors.white),
+                numButton("8", (Colors.white24)!, Colors.white),
+                numButton("9", (Colors.white24)!, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                numButton("4", (Colors.white24)!, Colors.white),
+                numButton("5", (Colors.white24)!, Colors.white),
+                numButton("6", (Colors.white24)!, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                numButton("1", (Colors.white24)!, Colors.white),
+                numButton("2", (Colors.white24)!, Colors.white),
+                numButton("3", (Colors.white24)!, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                numButton("C", Colors.grey, Colors.black),
+                numButton("=", Colors.deepPurpleAccent, Colors.white),
+                numButton("+", Colors.grey, Colors.black),
+              ],
+            ),
+            SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Logic
+  int firstNumber = 0;
+  int secondNumber = 0;
+  String result = "";
+  String text = "";
+  String operation = "";
+
+  void calculate(String btnText) {
+    if (btnText == "C") {
+      result = "";
+      text = "";
+      firstNumber = 0;
+      secondNumber = 0;
+    } else if (btnText == "+") {
+     firstNumber = int.parse(text);
+      result = "";
+      operation = btnText;
+    } else if (btnText == "=") {
+      secondNumber = int.parse(text);
+      if (operation == "+") {
+        result = (firstNumber + secondNumber).toString();
+      }
+    } else {
+      result = int.parse(text + btnText).toString();
+    }
+    setState(() {
+      text = result;
+    });
+  }
+}
